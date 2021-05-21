ef15e3868844838090178082600c6a673f5ebc17
Bananaperson21 <75265949+Bananaperson21@users.noreply.github.com>
2021-05-21 11:30:34 -0400

    Update 1.js

--- themes/1.js
+++ themes/1.js
@@ -1,1 +1,8 @@
+//background color
+var background = document.querySelector(".p2ZbV").style.backgroundColor = "#323232";
 
+//sound bar
+var soundbar = document.querySelector(".rd55Rb");for(var i in soundbar){soundbar[""+i+""].style.backgroundColor = "#ffc832";}
+
+//sound balls
+var soundballs = document.querySelector(".IisKdb");for(var i in soundball){soundball[""+i+""].style.backgroundColor = "#ffc832";}
